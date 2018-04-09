3b10aff2ea2eaf434affc2781fc4d0c4d480bac4
Leo Sjöberg <leo.sjoberg@gmail.com>
2018-04-09 17:08:21 +0100

    Use the correct classes when configuring handlers

--- src/Hindsight.php
+++ src/Hindsight.php
@@ -3,7 +3,10 @@
 namespace Hindsight;
 
 use Monolog\Logger;
+use Monolog\Handler\BufferHandler;
+use Monolog\Handler\WhatFailureGroupHandler;
 use Hindsight\Monolog\HindsightMonologHandler;
+use Decahedron\StickyLogging\StickyContextProcessor;
 
 class Hindsight
 {
