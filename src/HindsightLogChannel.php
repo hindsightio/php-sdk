36d89750455cc8f56b5ffd356f909f3ac4bd6c06
James Wallen-Jones <j.wallen.jones@googlemail.com>
2018-05-01 16:29:02 +0100

    Update log channel to get api_key from config

--- src/HindsightLogChannel.php
+++ src/HindsightLogChannel.php
@@ -8,6 +8,10 @@ class HindsightLogChannel
 {
     public function __invoke(array $config)
     {
-        return Hindsight::setup(new Logger(config('app.environment')), $config['api_key'], $config['level'] ?? Logger::DEBUG);
+        return Hindsight::setup(
+            new Logger(config('app.environment')),
+            config('hindsight.api_key'),
+            $config['level'] ?? Logger::DEBUG
+        );
     }
 }
